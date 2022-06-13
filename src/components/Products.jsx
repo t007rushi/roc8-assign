96396c21b95ee6bbf12308d63ace45f5f0af6e1f
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-06-13 18:07:01 +0530

    Products with cards

---
+++ src/components/Products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Card} from "./Card" 
+
+export const Products = ({filteredData}) => {
+    
+  return <div className="grid grid-cols-3 gap-6">
+    {filteredData.map(prod =>{
+        return(
+            <Card prod={prod}/>
+        )
+    })}
+  </div>;
+};
