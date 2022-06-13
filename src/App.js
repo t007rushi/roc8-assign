fa39a438dc471f5060e61bcba93815b2f76cd344
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-06-13 18:07:30 +0530

    Integrationg compoent into main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Sidebar } from "./components/Sidebar";
+import { Products } from "./components/Products";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [filtersArr, setFiltersArr] = useState([]);
+  const [search, setSearch] = useState("");
+  const filteredData =
+    filtersArr.length === 0
+      ? products
+      : products.filter(
+          (prod) =>
+            filtersArr.includes(prod.brand) ||
+            filtersArr.includes(prod.ram) ||
+            filtersArr.includes(prod.cpu)
+        );
+  const filteredSearchData =
+    search.length === 0
+      ? filteredData
+      : filteredData.filter((prod) => 
+          prod.title.toLowerCase().includes(search.toLowerCase())
+);
+
+const ClearFilter = () => {
+  setFiltersArr([]);
+  setSearch("")
+}
+
+  const checkBoxHandler = (val) => {
+    if (filtersArr.includes(val)) {
+      const updatedArr = filtersArr.filter((value) => val !== value);
+      setFiltersArr([...updatedArr]);
+    } else {
+      const updatedArr = [...filtersArr, val];
+      setFiltersArr([...updatedArr]);
+    }
+  };
+
+  const searchHandler = (sVal) => {
+    setSearch(sVal);
+  };
+
+  useEffect(() => {
+    fetch("https://62a717d997b6156bff8746f7.mockapi.io/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar search={search} searchHandler={searchHandler} />
+      <div className="flex gap-10">
+        <Sidebar checkBoxHandler={checkBoxHandler} filtersArr={filtersArr} ClearFilter={ClearFilter}/>
+        <Products filteredData={filteredSearchData} />
+      </div>
     </div>
   );
 }
