64f9abe9be1950ba88ca1569d2f9bdcdfbc02d29
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-06-14 12:30:48 +0530

    refactor : styling changes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useFilter } from "../context/filter-context";
 import { useProducts } from "../context/products-context";
 import { Card } from "./Card";
@@ -23,16 +23,18 @@ export const Products = () => {
         );
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 grid-flow-columns auto-columns-auto ">
+    <Fragment>
       {filteredSearchData.length === 0 ? (
         <span className="font-bold m-auto text-2xl">
           No Products Found Try again with different filters
         </span>
       ) : (
-        filteredSearchData.map((prod) => {
-          return <Card prod={prod} key={prod.price} />;
-        })
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 grid-flow-columns auto-columns-auto ">
+          {filteredSearchData.map((prod) => {
+            return <Card prod={prod} key={prod.price} />;
+          })}
+        </div>
       )}
-    </div>
+    </Fragment>
   );
 };
